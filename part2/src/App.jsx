93fcf5ccfa48e8cb666a92a1c230588dbf9341a2
redhama <jesse.lehosmaa@gmail.com>
2025-08-15 15:16:59 +0300

    avaimen lisäys kursseille

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
 
   return (
     <div>
-      {courses.map(course => <Course course={course} />)}
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
   )
 }
