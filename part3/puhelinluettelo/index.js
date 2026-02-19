0001e5eb5ee2947c83d9bd0de867efdc0ac1538a
redhama <jesse.lehosmaa@gmail.com>
2026-02-19 12:16:05 +0200

    part2, 3. front ja backend toimimaan

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -5,11 +5,12 @@ const app = express()
 morgan.token('body', function (request, response) {return JSON.stringify(request['body'])})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 app.use(express.json())
 
 let persons = [{id: "1", name: "Arto Hellas", number: "040-123456"}, {id: "2", name: "Ada Lovelace", number: "39-44-5323523"}, {id: "3", name: "Dan Abramov", number: "12-43-234345"}, {id: "4", name: "Mary Poppendieck", number: "39-23-6423122"}]
 
-app.get('/', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.send(persons)
 })
 
@@ -69,7 +70,7 @@ app.post('/api/persons/', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 }) 
