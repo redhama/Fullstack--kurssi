06356dd04471fe9289b022b3fadf0662f93172b3
redhama <jesse.lehosmaa@gmail.com>
2026-04-02 10:14:46 +0300

    ohjelman ajo linterin läpi

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -3,98 +3,98 @@ const morgan = require('morgan')
 const Phonenumber = require('./models/phonenumber')
 const app = express()
 
-morgan.token('body', function (request, response) {return JSON.stringify(request['body'])})
+morgan.token('body', function (request) {return JSON.stringify(request['body'])})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
-    Phonenumber.find({})
+  Phonenumber.find({})
     .then(phonenumbers => {
-        if(phonenumbers) {
-            response.json(phonenumbers)
-        } else {
-            response.status(404).end()
-        }
+      if(phonenumbers) {
+        response.json(phonenumbers)
+      } else {
+        response.status(404).end()
+      }
     })
 })
 
 app.get('/info', (request, response, next) => {
-    Phonenumber.find({}).then(phonenumbers => {
-        response.send(
-            `Phonebook has info for ${phonenumbers.length}</br>
+  Phonenumber.find({}).then(phonenumbers => {
+    response.send(
+      `Phonebook has info for ${phonenumbers.length}</br>
             ${new Date().toString()}`)
-        })
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Phonenumber.findById(request.params.id).then(phonenumber => {
-        if(phonenumber) {
-            response.json(phonenumber)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Phonenumber.findById(request.params.id).then(phonenumber => {
+    if(phonenumber) {
+      response.json(phonenumber)
+    } else {
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Phonenumber.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
+  Phonenumber.findByIdAndDelete(request.params.id)
+    .then(
+      response.status(204).end()
+    )
     .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const phonenumber = new Phonenumber({
-        name: body.name,
-        number: body.number
-    })
+  const phonenumber = new Phonenumber({
+    name: body.name,
+    number: body.number
+  })
 
-    phonenumber.save().then((savedphonenumber) => {
-        response.json(savedphonenumber)
-    })
+  phonenumber.save().then((savedphonenumber) => {
+    response.json(savedphonenumber)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Phonenumber.findById(request.params.id)
+  Phonenumber.findById(request.params.id)
     .then((phonenumber) => {
-        if (!phonenumber) {
-            return response.status(404).end()
-        }
+      if (!phonenumber) {
+        return response.status(404).end()
+      }
 
-        phonenumber.name = name
-        phonenumber.number = number
+      phonenumber.name = name
+      phonenumber.number = number
 
-        return phonenumber.save().then((updatedphonenumber) => {
-            response.json(updatedphonenumber)
-        })
+      return phonenumber.save().then((updatedphonenumber) => {
+        response.json(updatedphonenumber)
+      })
     })
     .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })        
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
@@ -102,6 +102,6 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-}) 
+  console.log(`Server running on port ${PORT}`)
+})
 
--- part3/puhelinluettelo/mongo.js
+++ part3/puhelinluettelo/mongo.js
@@ -5,40 +5,40 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url, {family:4})
-.then(result => {
+mongoose.connect(url, { family:4 })
+  .then(
     console.log('connected to MongoDB')
-})
-.catch((error) => {
+  )
+  .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 const phonenumberSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    number: String
+  id: Number,
+  name: String,
+  number: String
 })
 
 const Phonenumber = mongoose.model('Phonenumber', phonenumberSchema)
 
 const number = new Phonenumber({
-    id: 200,
-    name: process.argv[3],
-    number: process.argv[4]
-}) 
+  id: 200,
+  name: process.argv[3],
+  number: process.argv[4]
+})
 
-if (process.argv.length == 3) {
-    console.log("phonebook:")
-    Phonenumber.find({}).then(result => {
-        result.forEach(number => {
-            console.log(`${number.name} ${number.number}`)
-        })
-        mongoose.connection.close()
-    })    
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  Phonenumber.find({}).then(result => {
+    result.forEach(number => {
+      console.log(`${number.name} ${number.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else {
-    number.save().then(result => {
+  number.save().then(result => {
     console.log(`added ${result.name} number ${result.number} to phonebook`)
     mongoose.connection.close()
-})
+  })
 }
 
